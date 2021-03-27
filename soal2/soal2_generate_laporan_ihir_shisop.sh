cd9cb46c86098d5425dc7e193ff79804344396c1
Jundullah H. R <jundihanif77@gmail.com>
2021-03-27 21:58:44 +0700

    moved soal2 to it's folder

--- soal2/soal2_generate_laporan_ihir_shisop.sh
+++ soal2/soal2_generate_laporan_ihir_shisop.sh
@@ -1,38 +1,32 @@
+#!/bin/bash
+
 #2A
-@@ -20,7 +20,7 @@ awk '
+awk '
     END
     {
         printf("Transaksi terakhir dengan profit percentage terbesar 
         yaitu %d dengan persentase %d%%.\n", maksID, maks)
     }
 ' Laporan-TokoShisop.tsv>>hasil.txt
-' Laporan-TokoShiSop.tsv>>hasil.txt
 
-#2B
 awk '
-@@ -40,7 +40,7 @@ awk '
             printf("%s\n", data)
     }
     }
 ' Laporan-TokoShisop.tsv>>hasil.txt
-' Laporan-TokoShiSop.tsv>>hasil.txt
 
 #2C
 awk '
-@@ -69,7 +69,7 @@ awk '
     }
         printf("\n\nTipe segmen customer yang penjualannya paling sedikit 
         adalah %s dengan %d transaksi.\n\n", nama_segment, transaksi)
     }
 ' Laporan-TokoShisop.tsv>>hasil.txt
-' Laporan-TokoShiSop.tsv>>hasil.txt
 
 #2D
 awk '
-@@ -102,4 +102,4 @@ awk '
     }
         printf("Wilayah bagian (region) yang memiliki total keuntungan (profit) yang paling sedikit 
         adalah %s dengan total keuntungan %.2f.\n", nama_wilayah, minimal_profit)
     }
 ' Laporan-TokoShisop.tsv>>hasil.txt 
-' Laporan-TokoShiSop.tsv>>hasil.txt
